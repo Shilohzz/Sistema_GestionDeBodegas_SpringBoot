e7a857325c979e937bddc913997579698e72a7dd
Juan Bareno <barenojuan619@gmail.com>
2026-03-20 08:12:31 -0500

    I added the controller report title

--- src/main/java/com/logitrack/logitrack/controller/ReporteController.java
+++ src/main/java/com/logitrack/logitrack/controller/ReporteController.java
@@ -2,10 +2,14 @@ package com.logitrack.logitrack.controller;
 
 import com.logitrack.logitrack.DTO.response.ReporteMovimientosDTO;
 import com.logitrack.logitrack.repository.MovimientoRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+@Tag(name = "REPORTES DE MOVIMIENTOS", description = "Aquí se puede generar reportes para filtrar información.")
 @RestController
 @RequestMapping("/api/reportes")
 @RequiredArgsConstructor
@@ -14,6 +18,7 @@ public class ReporteController {
     private final MovimientoRepository movimientoRepository;
 
     // GET /api/reportes/movimientos
+    @Operation(summary = "")
     @GetMapping("/movimientos")
     public ResponseEntity<ReporteMovimientosDTO> reporteMovimientos() {
         ReporteMovimientosDTO reporte = new ReporteMovimientosDTO(
